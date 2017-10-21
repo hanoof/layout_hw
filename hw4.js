a3337f4163cac1dd923f1ee58c353f219aa1a2aa
Hanoof Alsuwailem <alhanoofsu@gmail.com>
2017-10-21 15:42:22 -0700

    COMMIT

---
+++ hw4.js
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<h2>Hanoof Alsuwailem HW4</h2>
+
+<script>
+const number=Number(prompt("Please Enter a Number Between 2 and 100"));
+
+if (number<2) || (number>10) {
+console.log(`${number} is invalid`);
+}
+
+else {
+
+console.log(`${number} is valid`);
+
+}
+
+</script>
+</body>
+</html>
